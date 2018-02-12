748eb836bf1ad4a44c811d0dbe62b01f209832e9
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-12 23:51:59 +1100

    refactor: add Button component import and css

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,32 +1,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
+import Button from '../components/Button/Button';
+import Input from '../components/Input/Input';
+import styles from './AddTodo.css'
 
 let AddTodo = ({ dispatch }) => {
   let input
-
   return (
-    <div>
-      <form
-        onSubmit={e => {
-          e.preventDefault()
-          if (!input.value.trim()) {
-            return
-          }
-          dispatch(addTodo(input.value))
-          input.value = ''
+    <form
+      className={styles.main}
+      onSubmit={e => {
+        e.preventDefault()
+        if (!input.value.trim()) {
+          return
+        }
+        dispatch(addTodo(input.value))
+        input.value = ''
+      }}
+    >
+      <Input
+        misc={node => {
+          input = node
         }}
-      >
-        <input
-          ref={node => {
-            input = node
-          }}
-        />
-        <button type="submit">
-          Add Todo
-        </button>
-      </form>
-    </div>
+      />
+
+      <Button type="submit" text="Add Todo" fontAwesomeIcon={'plus'} />
+    </form>
   )
 }
 AddTodo = connect()(AddTodo)
