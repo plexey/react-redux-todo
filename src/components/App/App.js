27718f49da53b781a8a52f5f08282d08ffbf1b34
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-12 23:45:23 +1100

    Refactor: convert App component to class and add css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { Component } from "react"
+import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import AddTodo from '../../containers/AddTodo'
 import VisibleTodoList from '../../containers/VisibleTodoList'
+import styles from './App.css';
 
-const App = () => (
-  <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
-  </div>
-)
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <Header />
+        <AddTodo />
+        <Footer />
+        <VisibleTodoList />
+      </div>
+    );
+  }
+}
 
-export default App
+export default App;
+
