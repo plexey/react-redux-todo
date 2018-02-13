1e7f3db62d0114427710864c710ee656c6e4e71a
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-13 17:18:23 +1100

    refactor: modify component layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react"
 import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
 import AddTodo from '../../containers/AddTodo'
 import VisibleTodoList from '../../containers/VisibleTodoList'
 import styles from './App.css';
+import TodoStats from "../../containers/TodoStats";
 
 class App extends Component {
   render() {
     return (
-      <div className={styles.container}>
+      <div className={styles.outer}>
         <Header />
-        <AddTodo />
-        <Footer />
-        <VisibleTodoList />
+        <div className={styles.container}>
+          <AddTodo />
+          <TodoStats />
+          <VisibleTodoList />
+        </div>
       </div>
+
     );
   }
 }
