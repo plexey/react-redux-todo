1052bf60fc36ef71096c50bc1e1d14962e08ffd1
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-13 17:18:59 +1100

    feat: add content and styling to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react"
+import styles from './Header.css';
 
 class Header extends Component {
   render() {
     return (
-      <div>
-
+      <div className={styles.container}>
+        <h1 className={styles.heading}>Redux Todo</h1>
       </div>
     );
   }
