8f205e62021fc4db0bac93d6f4d968eb79c251b0
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-15 12:26:09 +1100

    feat: add input field to edit existing todo

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,25 +1,13 @@
 import React, { Component } from "react"
 import { connect } from 'react-redux'
+import { editTodo, toggleTodo, removeTodo } from '../../modules/todo';
 import PropTypes from 'prop-types'
 import styles from './Todo.css';
 import classNames from 'classnames';
-import FontAwesome from 'react-fontawesome';
-
-const TodoText = ({ text, completed }) => (
-  <p
-    className={classNames({
-      [styles.text]: true
-    })}>
-    {text}
-  </p>
-)
 
 const TodoControl = ({ iconClasses, icon, action }) => (
   <div className={styles.control} onClick={action}>
-    <FontAwesome
-      className={iconClasses}
-      name={icon}
-    />
+    <i className={iconClasses}></i>
   </div>
 )
 
@@ -36,14 +24,30 @@ class Todo extends Component {
       active: val
     })
   }
+
+  handleChange = event => {
+    const { id, editTodo } = this.props;
+    editTodo({ id: id, text: event.target.value })
+  }
+
+  handleOnBlur = () => {
+    const { id, text, removeTodo } = this.props;
+    if (text.length === 0) {
+      removeTodo(id)
+    }
+  }
+
   render() {
     const { active } = this.state;
-    const { toggleTodo, removeTodo, completed, text } = this.props;
+    const { toggleTodo, removeTodo, id, completed, text } = this.props;
     const checkClasses = classNames({
       [styles.check]: true,
-      [styles.checkCompleted]: completed
+      "far fa-check-circle": completed === true,
+      "far fa-circle": completed === false,
+
     })
     const crossClasses = classNames({
+      "fas fa-times": true,
       [styles.cross]: true,
       [styles.crossVisible]: active
     })
@@ -57,24 +61,46 @@ class Todo extends Component {
         })}>
         <TodoControl
           iconClasses={checkClasses}
-          icon={completed ? 'check-square' : 'square'}
-          action={toggleTodo} />
-        <TodoText text={text} completed={completed} />
+          icon={completed ? 'check-circle' : 'fas-circle'}
+          action={() => toggleTodo(id)} />
+
+
+
+        <div className={styles.text}>
+          <input
+            className={classNames({
+              [styles.input]: true,
+              [styles.inputCompleted]: completed
+            })}
+            onChange={this.handleChange}
+            value={text}
+            onBlur={this.handleOnBlur} />
+        </div>
+
+
         <TodoControl
           iconClasses={crossClasses}
           icon={'times'}
-          action={removeTodo} />
+          action={() => removeTodo(id)} />
       </li>
     );
   }
 }
 
 Todo.propTypes = {
-  onClick: PropTypes.func.isRequired,
   completed: PropTypes.bool.isRequired,
   text: PropTypes.string.isRequired
 }
 
-Todo = connect()(Todo)
+const mapStateToProps = state => ({
+})
+
+const mapDispatchToProps = {
+  editTodo,
+  toggleTodo,
+  removeTodo
+}
+
+Todo = connect(mapStateToProps, mapDispatchToProps)(Todo)
 
 export default Todo;
